bd1bb469ca9c89ee788130841bd667536037c520
Oleksandr <fedorut20@gmail.com>
2023-07-08 16:06:39 +0300

    done

--- main.cpp
+++ main.cpp
@@ -2,12 +2,54 @@
 //
 
 #include <iostream>
+
 using namespace std;
 
-int main()
-{
+void deleteRow(int** arr, int rows, int cols, int rowToDelete) {
+    if (rowToDelete >= rows) {
+        return;
+    }
+
+    for (int i = rowToDelete; i < rows - 1; i++) {
+        for (int j = 0; j < cols; j++) {
+            arr[i][j] = arr[i + 1][j];
+        }
+    }
+
+    for (int i = 0; i < cols; i++) {
+        arr[rows - 1][i] = 0;
+    }
+}
+
+int main() {
+    const int ROWS = 3;
+    const int COLS = 3;
+
+    int** arr = new int* [ROWS];
+    for (int i = 0; i < ROWS; i++) {
+        arr[i] = new int[COLS];
+        for (int j = 0; j < COLS; j++) {
+            arr[i][j] = i * COLS + j;
+        }
+    }
+
+    deleteRow(arr, ROWS, COLS, 1);
+
+    for (int i = 0; i < ROWS - 1; i++) {
+        for (int j = 0; j < COLS; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < ROWS; i++) {
+        delete[] arr[i];
+    }
+    delete[] arr;
+
     return 0;
 }
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
