4f21755261da82b8486f67c77b782fbb53a0fdcd
Oleksandr <fedorut20@gmail.com>
2023-07-08 15:46:37 +0300

    task ex_1 done

--- main.cpp
+++ main.cpp
@@ -2,10 +2,62 @@
 //
 
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
-int main()
-{
+void addRandomRow(int**& arr, int& rows, int cols) {
+
+    int** newArr = new int* [rows + 1];
+    for (int i = 0; i < rows + 1; i++) {
+        newArr[i] = new int[cols];
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            newArr[i][j] = arr[i][j];
+        }
+    }
+
+    srand(time(0));
+    for (int j = 0; j < cols; j++) {
+        newArr[rows][j] = rand() % 100;
+    }
+
+    for (int i = 0; i < rows; i++) {
+        delete[] arr[i];
+    }
+    delete[] arr;
+
+
+    arr = newArr;
+    rows++;
+}
+
+int main() {
+    int rows = 2;
+    int cols = 3;
+    int** arr = new int* [rows];
+    for (int i = 0; i < rows; i++) {
+        arr[i] = new int[cols];
+    }
+
+    arr[0][0] = 1;
+    arr[0][1] = 2;
+    arr[0][2] = 3;
+    arr[1][0] = 4;
+    arr[1][1] = 5;
+    arr[1][2] = 6;
+
+    addRandomRow(arr, rows, cols);
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
     return 0;
 }
 
