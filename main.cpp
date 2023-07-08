fb97ede66840ecad774f8e311aff2e1f31cd9fd9
Oleksandr <fedorut20@gmail.com>
2023-07-08 15:35:00 +0300

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,13 @@
+// Multidimensional Dynamic Arrays.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
